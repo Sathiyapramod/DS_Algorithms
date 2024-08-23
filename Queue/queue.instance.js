fe1479fc5427623f2885bcb444a5626c14931433
Sathiyapramod Raghavendran <115987813+Sathiyapramod@users.noreply.github.com>
2024-08-23 11:34:34 +0530

    Create queue.instance.js

---
+++ Queue/queue.instance.js
@@ -0,0 +1,38 @@
+function queue(){
+  let items = [];
+  let front = 0;
+  let rear = -1;
+  let count= 0;
+
+  //enqueue 
+  this.enqueue = (el)=>{
+    // increment rear value 
+    // assign it to the el
+    // increment the count
+    items[++rear] = el;
+    count++;
+  }
+
+  //dequeue
+  this.dequeue = (el)=>{
+    // change the front value (increment)
+    // assign the incremented front index 
+    // change the count value (decrement)
+    // return the front element 
+    let current = front++;
+    let temp = item[current];
+    item[current] = null;
+    return temp;
+  }
+
+  //front
+  this.front = ()=> items[front];
+
+  //rear
+  this.rear = ()=> items[rear];
+
+  //isEmpty
+  this.empty = ()=> count === 0;
+
+  
+}
