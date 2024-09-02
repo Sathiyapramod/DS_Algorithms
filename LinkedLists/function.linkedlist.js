05175ed2bc0957a8e1e9daeb0215b749f96c25e6
Sathiyapramod Raghavendran <115987813+Sathiyapramod@users.noreply.github.com>
2024-09-02 16:01:44 +0530

    Update function.linkedlist.js

--- LinkedLists/function.linkedlist.js
+++ LinkedLists/function.linkedlist.js
@@ -1,33 +1,53 @@
-function linkedlist (){
-  
-  // initiating a node
-  let node = (el)=>{
-    this.element = el;
-    this.next = null;
-  }
-
-  //To keep track of the size    
-  let length = 0; 
-
-  //To keep track of the list
-  let head = null; 
-
-  // Add new item in the list 
-  this.append = (el)=>{
-    // adding a new node 
-    let newElem = new node(el);
-    let current;
-
-    if(head === null){
-      head = node;
+function linkedlist(){
+    let node = function(el){
+        this.element = el;
+        this.next = null;
     }
-    else {
+    
+    let length = 0;
+    
+    let head = null;
+    
+    
+    this.append = function(el){
+        // creating a new node
+        let newEl =  new node(el), current;
+        // check if head is null or some node exists 
+        if(!head){
+            // perform insert at a new list 
+            head = newEl
+        }
+        else {
+            // perform insert the desired tail - dead end 
+            current = head;
+            // traverse through the entire length of list 
+            while(current.next){
+                current = current.next;
+            }
+            // if next fails, stictch newEl to current.next
+            current.next = newEl
+        }
+        // increment the length however
+        length++;
+    }
+    
+    this.toArray = function(){
+      let arr = [],
       current = head;
-      while(current.next){
-        current = current.next
+      while(current){
+        arr.push(current.element);
+        current = current.next;
       }
-      current.next = node;
-    }
-    length++;
-  }
+      return arr;
+};
 }
+
+
+const ll = new linkedlist();
+ll.append(3);
+ll.append(4);
+ll.append('prashant');
+ll.append('anil');
+ll.append('29');
+console.log(ll.toArray());
+
