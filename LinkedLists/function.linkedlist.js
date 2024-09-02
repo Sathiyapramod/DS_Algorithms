d184e080b2eab8b6f0f80ebc731c0f4969f2d8e7
Sathiyapramod Raghavendran <115987813+Sathiyapramod@users.noreply.github.com>
2024-09-02 15:46:34 +0530

    Update function.linkedlist.js

--- LinkedLists/function.linkedlist.js
+++ LinkedLists/function.linkedlist.js
@@ -1,82 +1,33 @@
-class Node {
-  constructor(val) {
-    this.value = val;
+function linkedlist (){
+  
+  // initiating a node
+  let node = (el)=>{
+    this.element = el;
     this.next = null;
   }
-}
-class Linkedlist {
-  constructor() {
-    this.head = null;
-    this.size = 0;
-  }
 
-  insertNode(value) {
-    this.head = new Node(value, this.head);
-    this.size++;
-    
-  }
+  //To keep track of the size    
+  let length = 0; 
 
-  addElementToNode(value) {
-    let node = new Node(value);
-    if (!this.head) this.head; // if empty, create a new Node
+  //To keep track of the list
+  let head = null; 
+
+  // Add new item in the list 
+  this.append = (el)=>{
+    // adding a new node 
+    let newElem = new node(el);
+    let current;
+
+    if(head === null){
+      head = node;
+    }
     else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
+      current = head;
+      while(current.next){
+        current = current.next
       }
       current.next = node;
     }
-    return this.head;
-  }
-
-  insertNodeatFirst(head, value) {
-    if (this.head.value == head) {
-      let newNode = new Node(value);
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-    return this.head;
-  }
-
-  // insertNodeatLast(head, value) {
-  //   let temp = head;
-  //   while (temp.next != null) {
-  //     temp = temp.next;
-  //   }
-  //   let newNode = new Node(value);
-  //   temp.next = newNode;
-  //   return temp;
-  // }
-
-  printNode() {
-    let currentValue = this.head;
-    while (currentValue) {
-      console.log(currentValue.value);
-      currentValue = currentValue.next;
-    }
-  }
-  printList(flag) {
-    let current = flag.head;
-    console.log(current);
-    let temp = "";
-    while (current) {
-      temp += current.value + " ";
-      current = current.next;
-    }
-    console.log(temp);
-  }
-  getNode() {
-    let final = this.head;
-    return final;
+    length++;
   }
 }
-
-const ll = new Linkedlist();
-// ll.printNode();
-// let data = ll.getNode();
-ll.insertNode(10);
-ll.addElementToNode(20);
-ll.addElementToNode(30);
-// console.log(ll);
-
-ll.printList(ll);
