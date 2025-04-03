0e6e413edf750d91fa2415b875fe63a26c14e0ba
Sathiyapramod <sathiyapramod22@gmail.com>
2025-04-03 10:03:34 +0530

    feat: added oops concepts

--- oops/polymorphism.js
+++ oops/polymorphism.js
@@ -6,21 +6,27 @@ class Drive {
   }
 
   makeDrive() {
-    return `Driving a ${this.#vehicleType}`; // this is the default behaviour of the method
+    return `🛻 Driving a ${this.#vehicleType}`; // this is the default behaviour of the method
   }
 }
 
 class Bike extends Drive {
   constructor(bikeName) {
     super(bikeName);
+    this.bikeName = bikeName;
+  }
+  makeDrive() {
+    // this will perform a method over loading while inheriting the class
+    return `🚲 Riding a ${this.bikeName}`;
   }
 }
 
-class Car extends Drive {
-  constructor(carName) {
-    super(carName);
-  }
-}
-
-const testDrive = new Bike("Bike");
+// first instance - calling a Bike Class
+const testDrive = new Bike("Ducati");
 console.log(testDrive.makeDrive());
+// Output: Riding a Ducati
+
+const customDrive = new Drive("Ducati");
+console.log(customDrive.makeDrive());
+// here the method got over-rided
+// Output: Driving a Ducati
